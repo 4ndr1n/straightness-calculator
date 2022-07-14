52d252d6b77d1e5ce398835bfc9d1be93b5986a0
4ndr1n <47792683+4ndr1n@users.noreply.github.com>
2022-07-14 14:16:58 +0200

    Update Abweichung von gerader Strecke.py

--- Abweichung von gerader Strecke.py
+++ Abweichung von gerader Strecke.py
@@ -5,110 +5,121 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def getFile():
-    file = input()
+class getStuff:
+    def getFile():
+        file = input()
 
-    if file == "":
-        file = "/Users/Andrin/Desktop/activity_8914463883.gpx"
-    return file
+        if file == "":
+            file = "/Users/Andrin/Desktop/activity_8914463883.gpx"
+        return file
 
-def getData():
-    i = 0
-    file = open(getFile())
-    cont = file.readlines()
+    def getData():
+        i = 0
+        file = open(getStuff.getFile())
+        cont = file.readlines()
 
-    set = re.findall("\"+(-?\d+(\.\d+)?)\s*", str(cont))
+        set = re.findall("\"+(-?\d+(\.\d+)?)\s*", str(cont))
 
-    df = pd.DataFrame(set)
-    df.drop(columns=1, inplace=True)
-    df.drop([0,1],inplace=True)
-    df.reset_index(drop=True,inplace=True)
+        df = pd.DataFrame(set)
+        df.drop(columns=1, inplace=True)
+        df.drop([0,1],inplace=True)
+        df.reset_index(drop=True,inplace=True)
 
-    second_col = df[1::2]
-    second_col.dropna()
-    second_col.reset_index(drop=True,inplace=True)
-    df.drop(index=df.index[1::2],inplace=True)
-    df.dropna(inplace=True)
-    df.reset_index(drop=True,inplace=True)
-    df.columns = ['lat']
-    df["long"] = second_col
+        second_col = df[1::2]
+        second_col.dropna()
+        second_col.reset_index(drop=True,inplace=True)
+        df.drop(index=df.index[1::2],inplace=True)
+        df.dropna(inplace=True)
+        df.reset_index(drop=True,inplace=True)
+        df.columns = ['lat']
+        df["long"] = second_col
 
-    df = df.astype(float, errors="raise")
+        df = df.astype(float, errors="raise")
 
-    x = df.loc[:,"lat"]
-    y = df.loc[:,"long"]
+        x = df.loc[:,"lat"]
+        y = df.loc[:,"long"]
 
-    lat = gradient_calculator(x)
-    long = gradient_calculator(y)
-    return lat,long
+        lat = find.gradient_calculator(x)
+        long = find.gradient_calculator(y)
+        return lat,long
 
-def getTwoVals(x1,x2,x):
-    if x1 == 0:
-        x1 = x
-    elif x2 == 0:
-        x2 = x
-    else: 
-        x1 = x2
-        x2 = x
-    return x1, x2
+    def getTwoVals(x1,x2,x):
+        if x1 == 0:
+            x1 = x
+        elif x2 == 0:
+            x2 = x
+        else: 
+            x1 = x2
+            x2 = x
+        return x1, x2
 
-def gradient_calculator(x_y_val):
-    z = []
-    x1=0
-    x2=0
-    for x in x_y_val:
-        x1, x2 = getTwoVals(x1,x2,x)
-        if x1 == x:
-            pass
-        else:
-            d = x2 - x1    
-            z.append(d)
-    return z
+class  find:
+    def gradient_calculator(x_y_val):
+        z = []
+        x1=0
+        x2=0
+        for x in x_y_val:
+            x1, x2 = getStuff.getTwoVals(x1,x2,x)
+            if x1 == x:
+                pass
+            else:
+                d = x2 - x1    
+                z.append(d)
+        return z
 
-def findGradOverTime(lat,long):
-    gradOverTime = []
-    n = 0
-    for x in lat:
-        n += 1
+    def findGradOverTime(lat,long):
+        gradOverTime = []
+        n = 0
+        for x in lat:
+            n += 1
 
-    for x in range(n):
-        if long[x] == 0 or long[x] == 0.0:
-            pass
-        else:
-            gradOverTime.append(lat[x] / long[x])
-    return gradOverTime
+        for x in range(n):
+            if long[x] == 0 or long[x] == 0.0:
+                pass
+            else:
+                gradOverTime.append(lat[x] / long[x])
+        # outToCsv(gradOverTime)
+        return gradOverTime
 
-def findTurn(grad):
-    manualIndex = 0
-    indexList = []
-    prunedIndex = []
-    x1 = 0
-    x2 = 0
-    dif = 0
+    def findTurn(grad):
+        manualIndex = 0
+        indexList = []
+        prunedIndex = []
+        x1 = 0
+        x2 = 0
+        dif = 0
 
-    for x in grad:
-        manualIndex += 1
-        if x < 0:
-            indexList.append(manualIndex)
-    for x in indexList:
-        x1,x2 = getTwoVals(x1,x2,x)
-        if x1 == x:
-            prunedIndex.append(x)
-            pass
-        else:
-            dif = x2 - x1
+        for x in grad:
+            manualIndex += 1
+            if x < 0:
+                indexList.append(manualIndex)
+        for x in indexList:
+            x1,x2 = getStuff.getTwoVals(x1,x2,x)
+            if x1 == x:
+                prunedIndex.append(x)
+                pass
+            else:
+                dif = x2 - x1
 
-        if dif > 20:
-            prunedIndex.append(x)
-        else:
-            pass
-    print(prunedIndex)
+            if dif > 30:
+                prunedIndex.append(x)
+            else:
+                pass
+        print(prunedIndex)
 
+def outToCsv(gradOverTime):
+    f = open("/Users/Andrin/Desktop/sarnen.csv","w")
+
+    for x in gradOverTime:
+        y = str(x)
+        z = y + "\n"
+        f.write(z)
+    f.write(gradOverTime)
 
 def main():
-    lat, long = getData()
-    gradOverTime = findGradOverTime(lat,long)
-    turn = findTurn(gradOverTime)
+    lat, long = getStuff.getData()
+    gradOverTime = find.findGradOverTime(lat,long)
+    turn = find.findTurn(gradOverTime)
 
 
 if __name__ == "__main__":
