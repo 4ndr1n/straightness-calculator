28c88df88f1191b99160fd9b8544e542684f74ca
4ndr1n <47792683+4ndr1n@users.noreply.github.com>
2022-11-09 08:42:36 +0100

    gradient descent implemented

--- ML model.py
+++ ML model.py
@@ -1,6 +1,7 @@
 import re
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 class getStuff:
@@ -85,8 +86,8 @@ class ML:
         return Z > 0
 
     
-    def back_prop(Z1,A1,Z2,A2,W2,X,Y,m):
-        dZ2 = ML.ReLU_deriv(Z2) - Y
+    def back_prop(Z1,A1,A2,W2,X,Y,m):
+        dZ2 = A2 - Y
         dW2 = 1 / m * dZ2.dot(A1.T)
         db2 = 1 / m * np.sum(dZ2)
         dZ1 = W2.T.dot(dZ2) * ML.ReLU_deriv(Z1)
@@ -94,12 +95,66 @@ class ML:
         db1 = 1 / m * np.sum(dZ1)
         return dW1, db1, dW2, db2
 
-        
+
+    def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+        W1 = W1 - alpha * dW1
+        b1 = b1 - alpha * db1    
+        W2 = W2 - alpha * dW2  
+        b2 = b2 - alpha * db2    
+        return W1, b1, W2, b2
+
+
+    def get_predictions(A2):
+        return np.argmax(A2, 0)
+
+    def get_accuracy(predictions, Y):
+        print(predictions, Y)
+        return np.sum(predictions == Y) / Y.size
+
+    def gradient_descent(X,Y,alpha, iterations):
+        W1, b1, W2, b2, = ML.init_params()
+        for i in range(iterations):
+            Z1, A1, Z2, A2 = ML.forward_prop(W1,b1,W2,b2,X)
+            dW1, db1, dW2, db2 = ML.backwards_prop(Z1,A1,Z2,A2,W1,W2,X,Y)
+
+            W1,b1,W2,b2 = ML.update_params(W1,b1,W2,b2,dW1,db1,dW2,db2,alpha)
+
+            if i % 10 == 0:
+                print("Iteration:", i)
+                predictions = ML.get_predictions(A2)
+                print(ML.get_accuracy(predictions,Y))
+        return W1,b1,W2,b2
+
+
+class test:
+
+    def make_predictions(X,W1,b1,W2,b2):
+        _,_,_,A2 = ML.forwards_prop(W1,b1,W2,b2,X)
+        predictions = ML.get_predictinos(A2)
+        return predictions
+
+    def test_predictions(index, W1, b1, W2, b2,X_train,Y_train):
+        current_image = X_train[:,index,None]
+        prediction = ML.make_predictions(X_train[:, index, None], W1,b1,W2,b2)
+        label = Y_train[index]
+        print("Prediction: ",prediction)
+        print("Label:",label)
+
+        current_image = current_image.reshape((28,28))* 255
+        plt.gray()
+        plt.imshow(current_image, interpolation='nearest')
+        plt.show()
+
+   
+
+
 
 
 def main():
     x,y = getStuff.getData()
     X_dev,Y_dev,X_train,Y_train = ML.dataprep(x,y)
+
+    W1,b1,W2,b2 = ML.gradient_descent(X_train,Y_train, 0.01,500)
 
 if __name__ == "__main__":
     main()
